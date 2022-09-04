dd59a3d7c333598b75bc63592e61f7f1434abd8a
Alla <allakortunova@yandex.ru>
2022-09-04 22:56:54 +0300

    HT001

---
+++ HOMETASKS001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write (" Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+Console.WriteLine($"Число {number} является четным ");
+Console.WriteLine("да");
+}
+else
+{
+Console.WriteLine($"Число {number} является нечетным ");
+Console.WriteLine("нет");
+}
